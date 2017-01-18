72f0e28c0e5f9d35d035a0e0e9dd382ba3c48f23
Rompei <rompei0924@gmail.com>
2017-01-18 13:20:40 +0900

    Add images.

--- html/main.go
+++ html/main.go
@@ -18,16 +18,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	templ, err := template.ParseFiles("templates/ts3widget.html", "templates/channel.html")
+	templ, err := template.ParseFiles("templates/index.html", "templates/ts3widget.html", "templates/channel.html")
 	if err != nil {
 		panic(err)
 	}
-	if err = templ.ExecuteTemplate(w, "ts3widget", server); err != nil {
+	if err = templ.ExecuteTemplate(w, "index", server); err != nil {
 		panic(err)
 	}
 }
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil)
 }
